1f00af6a9a2119a9799b8debc0aae1eeb08e5a7a
TNoLisme <thinh01092005@gmail.com>
2024-12-16 14:21:38 +0700

    feature/thinh/frontend-showlist-category

--- backend/src/main/java/com/example/QuanLyThuVien/Repo/CategoryRepository.java
+++ backend/src/main/java/com/example/QuanLyThuVien/Repo/CategoryRepository.java
@@ -1,7 +1,14 @@
 package com.example.QuanLyThuVien.Repo;
 
+import com.example.QuanLyThuVien.DTO.CategoryDTO;
 import com.example.QuanLyThuVien.Entity.Category;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+	 @Query("SELECT new com.example.QuanLyThuVien.DTO.CategoryDTO(c.id, c.name) FROM Category c")
+	    public List<CategoryDTO> getAllCategories();
 }
