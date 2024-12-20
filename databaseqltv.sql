70aac20a86c6f34c6746e97945a23688ac06fc81
TNoLisme <167105259+TNoLisme@users.noreply.github.com>
2024-12-20 22:54:30 +0700

    Update databaseqltv.sql

--- databaseqltv.sql
+++ databaseqltv.sql
@@ -31,15 +31,6 @@ CREATE TABLE `admins` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
-
-INSERT INTO fines (fine_reason, fine_amount) VALUES
-('Late book return', 10.00),              -- Phạt 10.00 vì trả sách trễ
-('Damaged book', 20.00),               -- Phạt 20.00 vì sách bị hư hỏng
-('Lost book', 30.00);                -- Phạt 30.00 vì đánh mất sách
-
-INSERT INTO admins (email, full_name, password) VALUES
-('admin', "Administrator", '123');
-
 --
 -- Table structure for table `bookcategory`
 --
@@ -197,3 +188,13 @@ CREATE TABLE `reviews` (
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2024-12-19 13:52:37
+
+
+
+INSERT INTO fines (fine_reason, fine_amount) VALUES
+('Late book return', 10.00),              -- Phạt 10.00 vì trả sách trễ
+('Damaged book', 20.00),               -- Phạt 20.00 vì sách bị hư hỏng
+('Lost book', 30.00);                -- Phạt 30.00 vì đánh mất sách
+
+INSERT INTO admins (email, full_name, password) VALUES
+('admin', "Administrator", '123');
